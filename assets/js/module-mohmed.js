519acb0624ae4193070c85ade26983855c082969
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-15 22:32:46 -0500

    added google maps logic.

--- assets/js/module-mohmed.js
+++ assets/js/module-mohmed.js
@@ -1,8 +1,19 @@
-<<<<<<< HEAD
-//Mohmed
-=======
 //Mohmed google maps api
 
-<iframe width="600" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/search?q=bars+near+Chicago,+IL,+USA&key=..." allowfullscreen></iframe>
->>>>>>> 7b2115f890e8bc41684ee0fa938a273b45b96ade
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position)
+            $(`#g-maps`).append(`
+        <iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/search?q=bars%20near%20me&key=AIzaSyDjJeZWvxxMbymDJSMvXpXphLrD4lxDP84" allowfullscreen></iframe>
+        `)
+        });
+
+    } else {
+        $(`#g-maps`).text("Geolocation is not supported by this browser.")
+    }
+}
+
+getLocation();
