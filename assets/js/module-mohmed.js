ccc63c54679c726a77630633b9990687879211de
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-18 19:00:12 -0500

    Google places api cross origin error

--- assets/js/module-mohmed.js
+++ assets/js/module-mohmed.js
@@ -1,20 +1,59 @@
 //Mohmed google maps api
+$(document).ready(function () {
+
+    var lat;
+    var long;
+    var queryURL;
+    var data = [];
 
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            console.log(position)
-            $(`#g-maps`).append(`
-        <iframe width="745" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/search?q=bars%20near%20me&key=AIzaSyDjJeZWvxxMbymDJSMvXpXphLrD4lxDP84" allowfullscreen></iframe>
-        `)
-        });
-        $(`#g-maps`).attr(`style`, `text-align: center`)
+    $('.sidenav').sidenav();
 
-    } else {
-        $(`#g-maps`).text("Geolocation is not supported by this browser.")
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+
+                lat = position.coords.latitude
+                long = position.coords.longitude
+                queryURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDjJeZWvxxMbymDJSMvXpXphLrD4lxDP84`
+
+                //nearbylocaion(lat, long);
+                
+                //radius is in meters
+
+            });
+
+        } else {
+            $(`#g-maps`).text("Geolocation is not supported by this browser.")
+        }
     }
+
+function gmap(){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.results.length)
+        var resultlength = response.results.length
+        console.log(`resultslength = ${resultlength}`)
+
+        for (var i = 0; i < resultlength; i++) {
+            data.push({
+                name: response.results[i].name,
+                opennow: response.results[i].opening_hours.open_now,
+                photo: response.results[i].photos.html_attributions
+
+            })
+            console.log(`data: ${data}`)
+        }
+        console.log(data)
+    })
+
 }
 
-getLocation();
+    getLocation();
+    gmap();
+    
+    
+
+});
