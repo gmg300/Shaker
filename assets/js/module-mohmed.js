5026495ec23a9d7358160f25668f26c42051f664
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-19 21:20:05 -0500

    tried loading google maps with methods but still not worki ng

--- assets/js/module-mohmed.js
+++ assets/js/module-mohmed.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     var lat;
     var long;
     var queryURL;
@@ -15,7 +16,45 @@ $(document).ready(function () {
                 lat = position.coords.latitude
                 long = position.coords.longitude
                 queryURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDjJeZWvxxMbymDJSMvXpXphLrD4lxDP84`
-                gmap();
+                console.log(queryURL)
+                // gmap();
+                //  beg 
+                var map;
+                var service;
+                var infowindow;
+
+                function initialize() {
+                    var pyrmont = new google.maps.LatLng(41, -87);
+
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: pyrmont,
+                        zoom: 15
+                    });
+
+                    var request = {
+                        location: pyrmont,
+                        radius: '500',
+                        types: ['bar']
+                    };
+
+                    service = new google.maps.places.PlacesService(map);
+                    service.nearbySearch(request, callback);
+                }
+                google.maps.event.addDomListener(window, "load", initialize);
+
+                function callback(results, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        for (var i = 0; i < results.length; i++) {
+                            var place = results[i];
+                            console.log(place.name);
+                            console.log(place.place_id);
+                            createMarker(results[i]);
+                        }
+                    }
+                }
+                initialize();
+
+                // end
             });
 
         } else {
@@ -26,9 +65,11 @@ $(document).ready(function () {
     function gmap() {
 
         $.ajax({
+            crossDomain: true,
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            console.log(response)
 
             for (var i = 0; i < response.results.length; i++) {
                 data.push({
